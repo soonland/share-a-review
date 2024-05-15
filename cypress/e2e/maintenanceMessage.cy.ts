6db32f8663fea04a7f89bd664af39720637c2479
ginosanterre <168988326+ginosanterre@users.noreply.github.com>
2024-05-15 13:26:16 -0400

    test: Add cy.intercept to maintenanceMessage.cy.ts

--- cypress/e2e/maintenanceMessage.cy.ts
+++ cypress/e2e/maintenanceMessage.cy.ts
@@ -1,8 +1,35 @@
-describe("Test for maintenance message", () => {
-  it("Check if the maintenance message is displayed", () => {
+describe("Test for maintenance message (maintenanceMode=true)", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "http://localhost:3000/api/maintenance", (req) => {
+      req.reply({
+        statusCode: 200,
+        body: {
+          maintenanceMode: "true",
+        },
+      });
+    });
     cy.visit("http://localhost:3000");
+  });
 
-    // si le site est en maintenance alors
+  it("Check if the maintenance message is displayed", () => {
     cy.get(".MuiBackdrop-root > .MuiBox-root").should("exist");
+  });
+});
+
+describe("Test for maintenance message (maintenanceMode=false)", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "http://localhost:3000/api/maintenance", (req) => {
+      req.reply({
+        statusCode: 200,
+        body: {
+          maintenanceMode: "false",
+        },
+      });
+    });
+    cy.visit("http://localhost:3000");
+  });
+
+  it("Check if the maintenance message is not displayed", () => {
+    cy.get(".MuiBackdrop-root > .MuiBox-root").should("not.exist");
   });
 });
