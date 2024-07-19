b6f46f52b4196df4d744a6824ff5137cc3dc11b3
soonland <76574139+soonland@users.noreply.github.com>
2024-07-19 09:11:58 -0400

    chore: cleaned code

--- pages/api/items/create.ts
+++ pages/api/items/create.ts
@@ -4,16 +4,16 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     console.log("req.body", req.body);
     const { name, categoryId } = req.body;
+    const description = { ...req.body, categoryId: undefined };
     try {
       const slug = name.toLowerCase().replace(/\s/g, "-");
       const newReview = await pool.query(
-        "INSERT INTO items (name, slug, category_id, description, date_created) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-        [name, slug, parseInt(categoryId, 10), JSON.stringify(req.body), new Date()],
+        "INSERT INTO items (name, slug, category_id, description) VALUES ($1, $2, $3, $4) RETURNING *",
+        [name, slug, parseInt(categoryId, 10), JSON.stringify(description)],
       );
       res.status(201).json(newReview.rows[0]);
     } catch (error) {
-      console.error("Failed to submit review", error);
-      res.status(500).json({ error: "Failed to submit review" });
+      res.status(500).json({ error: "Failed to create item" });
     }
   } else {
     res.status(405).json({ error: "Method not allowed" });
