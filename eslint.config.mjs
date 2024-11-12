283823fdfe1843042fa71a759e77b8a8496ef7ce
soonland <76574139+soonland@users.noreply.github.com>
2024-11-12 07:42:27 -0500

    chore: formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,44 +1,49 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends(
+const config = [
+  ...compat.extends(
     "next/core-web-vitals",
     "eslint:recommended",
     "plugin:prettier/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-), {
+  ),
+  {
     rules: {
-        "prettier/prettier": ["error", {
-            printWidth: 120,
-        }],
+      "prettier/prettier": [
+        "error",
+        {
+          printWidth: 120,
+        },
+      ],
 
-        "import/order": ["error", {
-            groups: [
-                "builtin",
-                "external",
-                "internal",
-                ["sibling", "parent"],
-                "index",
-                "unknown",
-            ],
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", ["sibling", "parent"], "index", "unknown"],
 
-            "newlines-between": "always",
+          "newlines-between": "always",
 
-            alphabetize: {
-                order: "asc",
-                caseInsensitive: true,
-            },
-        }],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
-}];
+  },
+];
+
+export default config;
